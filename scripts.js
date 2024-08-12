f87e73d80cd91308012e783410966f35f99a935f
anurag-semwal <anuragsemwal.workmail@gmail.com>
2024-08-13 00:08:08 +0530

    Display winner and reset board on win

--- scripts.js
+++ scripts.js
@@ -3,6 +3,116 @@ TO DO
 - Remove the cell divs from html and render them using javascript 
 */
 
+const Player = (function () {
+  // Private
+  function createPlayer(marker) {
+    let moves = []; // stores all the indexes at which the player places his/her marker
+
+    function get() {
+      return { marker, moves };
+    }
+    function updateMoves(cellID) {
+      moves.push(cellID);
+    }
+    function resetPlayerMoves() {
+      moves = [];
+    }
+
+    return { get, updateMoves, resetPlayerMoves };
+  }
+
+  const playerX = createPlayer("X");
+  const playerO = createPlayer("O");
+
+  /**
+   * const { getPlayer: getPlayerX, updatePlayerMoves: updatePlayerMovesX } = createPlayer("X");
+   * const { getPlayer: getPlayerO, updatePlayerMoves: updatePlayerMovesO } = createPlayer("O");
+   *
+   * the above statements take the value of getPlayer from the object returned by createPlayer()
+   * and then store it inside getPlayerX and getPlayerO respectively
+   * this is called reverse destructuring
+   */
+
+  return { playerX, playerO };
+})();
+
+const Board = (function (doc) {
+  //  Winning combinations in a tic tac toe game
+  const winnningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // contains the info about which cell on the board has which marker
+  const boardStatus = new Array(9).fill("");
+  let currentPlayer = Player.playerX;
+
+  // Private functions
+  function registerMove(cellID, currPlayer) {
+    boardStatus[cellID] = currPlayer.get().marker;
+    currPlayer.updateMoves(cellID);
+  }
+  function renderBoard() {
+    boardStatus.forEach((marker, index) => {
+      let currentCell = doc.getElementById(`${index}`);
+      currentCell.textContent = marker;
+    });
+  }
+  function isWinner(currPlayer) {
+    return winnningCombinations.some((combination) => combination.every((index) => currPlayer.get().moves.includes(index)));
+  }
+  function nextPlayer(currPlayer) {
+    return currPlayer.get().marker === Player.playerX.get().marker ? Player.playerO : Player.playerX;
+  }
+  function displayWinner(winner) {
+    const heading = doc.querySelector(".heading-container > h1");
+    heading.textContent = `The winner is Player ${winner.get().marker}`;
+
+    resetBoard();
+  }
+
+  function handleClick(e) {
+    registerMove(Number(e.target.id), currentPlayer);
+    renderBoard();
+    if (isWinner(currentPlayer)) {
+      displayWinner(currentPlayer);
+    } else {
+      currentPlayer = nextPlayer(currentPlayer);
+    }
+  }
+  function addEventsToBoardCells() {
+    let cells = doc.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("click", handleClick);
+    });
+  }
+  function removeEventsFromBoardCells() {
+    let cells = doc.querySelector(".cell");
+    cells.forEach((cell) => {
+      cell.removeEventListener("click", handleClick);
+    });
+  }
+
+  // Public functions
+  function init() {
+    addEventsToBoardCells();
+  }
+
+  function resetBoard() {
+    boardStatus.fill("");
+    renderBoard();
+    removeEventsFromBoardCells();
+  }
+
+  return { init, resetBoard };
+})(document);
+
 const GameController = (function (doc) {
   /*
 
@@ -22,60 +132,6 @@ const GameController = (function (doc) {
   }
 
   return { startGame };
-})(document);
-
-const Player = (function () {
-  function createPlayer(marker) {
-    return { marker, moves: [] }; // moves[] stores the index at which the player places his/her marker
-  }
-
-  return { createPlayer };
-})();
-
-const Board = (function (doc) {
-  /**
-   * Board() does the following things
-   */
-
-  // DOM access variables
-  const board = doc.querySelector(".board");
-  const boardStatus = new Array(9).fill(""); // store moves by X and O together at the respective index - used to render board
-  const playerX = Player.createPlayer("X");
-  const playerO = Player.createPlayer("O");
-  let currentPlayer = playerX;
-
-  // utility functions
-  function registerMove(cellID) {
-    boardStatus[cellID] = currentPlayer.marker;
-    currentPlayer.moves.push(cellID);
-  }
-  function switchCurrentPlayer(currPlayer) {
-    currentPlayer = currPlayer.marker === playerX.marker ? playerO : playerX;
-  }
-  function renderBoard() {
-    boardStatus.forEach((marker, index) => {
-      let currentCell = doc.getElementById(`${index}`);
-      currentCell.textContent = marker;
-    });
-  }
-  function addEventsToBoardCells() {
-    let cells = board.querySelectorAll(".cell");
-    cells.forEach((cell) => {
-      cell.addEventListener("click", (e) => {
-        console.log(e.target.id);
-        registerMove(Number(e.target.id));
-        renderBoard();
-        switchCurrentPlayer(currentPlayer);
-      });
-    });
-  }
-
-  // UI functions
-  function init() {
-    addEventsToBoardCells();
-  }
-
-  return { init };
 })(document);
 
 GameController.startGame();
